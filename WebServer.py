4db45c04de1413601fb243269a6233242d4422e0
Marcel <Prakashmarcel@gmail.com>
2025-08-06 12:23:18 +1200

    Added Partial HTTP Parsing + Flask API

--- WebServer.py
+++ WebServer.py
@@ -1,6 +1,11 @@
 #HTTP Server Python
 import socket
 import os
+import io
+import sys
+
+#Import Flask App
+from app import app as flask_app
 
 class http_server_program:
     """
@@ -21,6 +26,16 @@ class http_server_program:
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.host, self.port))
 
+        #Return Headers Set By Web Framework/Web App
+        self.headers_set = []
+
+        #Set Server name and port for Environ dictionary
+        self.server_name = self.host
+        self.server_port = self.port
+
+    def set_app(self,application):
+        self.application= application
+
    #TCP Socket Listener
     def serve_forever(self):
         """
@@ -33,13 +48,13 @@ class http_server_program:
 
         while True:
             #Wait for client connections
-            client_connection, client_address = self.socket.accept()
+            self.client_connection, client_address = self.socket.accept()
             print (f"Accepted Connection From: {client_address}")
 
             #Handle the client's request
-            self.handle_request(client_connection)
+            self.handle_request(self.client_connection)
 
-        
+
 
     #HTTP Handler
     def handle_request(self, connection):
@@ -48,41 +63,86 @@ class http_server_program:
         """
         #Get the client request
         request_data = connection.recv(1024).decode('utf-8')
+        self.request_data = request_data
         print ("---Request Start---")
         print (request_data.strip())
         print ("---Request End---")
 
-        #Parse HTTP headers to get the requested filename
-        headers = request_data.split('\r\n')
-        filename = headers[0].split()[1]
-
-        #Get the content of the file
-        if filename == '/':
-            filename = '/index.html'
+        self.parse_request(request_data)
         
-        #Construct Full File Path
-        filepath = os.path.join(self.web_root, filename.lstrip('/'))
+        #Construct envrioment dictionary using request data
+        env = self.get_environ()
 
-        #Check for path traversal attempts
-        if not os.path.abspath(filepath).startswith(os.path.abspath(self.web_root)):
-            raise FileNotFoundError("Path Traversal Attempt")
+        #Call Application
+        result = self.application(env, self.start_response)
 
-        #Read the file and prepare response
-        try:
-            with open(filepath, 'r') as f:
-                content = f.read()
+        #Construct Response
+        self.finish_response(result)
 
-            http_response = 'HTTP/1.1 200 ok\r\n\r\n' + content
-        except FileNotFoundError:
-            http_response = 'HTTP/1.1 404 NOT FOUND\r\n\r\nFile Not Found'
-        except IOError:
-            http_response = 'HTTP/1.1 500 Internal Server Error\r\n\r\n Could not read file'
+    def parse_request(self, text):
+        #Parse HTTP headers to get the requested filename
+        request_line = text.splitlines()[0]
+        request_line = request_line.rstrip('\r\n')
             
-        #Send HTTP Response
-        connection.sendall(http_response.encode('utf-8'))
-        connection.close()
+        #Break down the request line into components
+        (self.request_method, #GET
+        self.path, #Hello
+        self.request_version #HTTP 1.1
+        ) = request_line.split()
+            
+    def get_environ(self):
+        env = {}
+        #Required WSGI Variables
+        env['wsgi.version'] = (1, 0)
+        env['wsgi.url_scheme'] = 'http'
+        env['wsgi.input'] = io.StringIO(self.request_data)
+        env['wsgi.errors'] = sys.stderr
+        env['wsgi.multithread'] = False
+        env['wsgi.multiprocess'] = False
+        env['wsgi.run_once'] = False
+        #Required CGI Variables
+        env['REQUEST_METHOD'] = self.request_method #GET
+        env['PATH_INFO'] = self.path #/hello
+        env['SERVER_NAME'] = self.server_name #LocalHost
+        env['SERVER_PORT'] = str(self.server_port) #8888
+        return env
+    
+    def start_response(self,status,response_headers, exc_info=None):
+        #Add Necessary Server Headers
+        server_headers = [
+            ('Date', 'Mon, 06 August 10:45:01 NZST'),
+            ('Server','WSGIServer 0.2')
+        ]
+        self.headers_set = [status, response_headers + server_headers]
+        #To Adhere to WSGI Standards we must return a 'Write' callable this will be ignored for now
+        #return self.finish_response 
+
+
+    def finish_response(self, result):
+        try:
+            status, response_headers = self.headers_set
+            response = f'HTTP/1.1 {status}\r\n'
+            for header in response_headers:
+                response += '{0}: {1}\r\n'.format(*header)
+            response += '\r\n'
+            for data in result:
+                response_bytes = data
+                self.client_connection.sendall(response_bytes)
+        finally:
+            self.client_connection.close()
+
+def make_server(server_address, application):
+    host, port = server_address
+    server = http_server_program(host, port)
+    server.set_app(application)
+    return server
+
+
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
 
 if __name__ == '__main__':
-    #create an instance of the server and run it
-    server = http_server_program()
-    server.serve_forever()
+ httpd = make_server(SERVER_ADDRESS, flask_app)
+ print(f'WSGIServer: Serving HTTP on port {PORT}... \n')
+ httpd.serve_forever()
+
+ 
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+#New route for the root URL
+@app.route('/', methods=['GET'])
+def index():
+    return "<h1>Welcome to your API-Driven web Server!</h1><p>Navigate to /messages to see API data</p>"
+
+#This will be the API end point
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = [
+        {"id": 1, "text": "Hello, this is the first message!"},
+        {"id": 2, "text": "Welcome to the message board API."}
+    ]
+
+    
+    return jsonify(messages)
+if __name__ == '__main__':
+    pass 
